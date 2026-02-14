5117173b1f7e8c1c20077263870c67b8d41137b6
jarcities <Hawaiian647@gmail.com>
2026-02-14 12:42:17 -0800

    500 epochs, all cpu, none.

--- mlp_training/run.sh
+++ mlp_training/run.sh
@@ -1,2 +1,2 @@
-python3 trainMLP.py --epochs=500 --bit=64 --num-cpus=0 --use-cuda > out 2> err
+python3 trainMLP.py --epochs=500 --bit=64 --num-cpus=0 > out 2> err
 cat out | grep "tensor(" | cut -d "(" -f2 | cut -d "," -f1 | cut -d ")" -f1 > out2
