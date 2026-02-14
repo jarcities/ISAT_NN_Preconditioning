9b20b3f4ecf61b6383ac1f48a2ac43e40e267120
jarcities <Hawaiian647@gmail.com>
2026-02-13 20:36:51 -0800

    updated instruction for out2.

--- mlp_training/run.sh
+++ mlp_training/run.sh
@@ -1,2 +1,2 @@
-python3 trainMLP.py --epochs=100 --bit=64 --num-cpus=4 > out 2> err
-cat out | grep "tensor(" | cut -d "(" -f2 | cut -d ")" -f1 > out2
+python3 trainMLP.py --epochs=100 --bit=64 --num-cpus=4 --use-cuda > out 2> err
+cat out | grep "tensor(" | cut -d "(" -f2 | cut -d "," -f1 | cut -d ")" -f1 > out2
